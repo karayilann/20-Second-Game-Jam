7020d129772636de614b312f425e339e4b07df93
Rakun665 <yemir.yilmaz2@gmail.com>
2022-11-04 00:15:18 +0300

    CreateBlock Code

--- 20 Seconds Game Jam/Assets/Scripts/InsantiateBlocks.cs
+++ 20 Seconds Game Jam/Assets/Scripts/InsantiateBlocks.cs
@@ -6,21 +6,30 @@ public class InsantiateBlocks : MonoBehaviour
 {
     [SerializeField] GameObject ClickBlock;
     [SerializeField] GameObject PushBlocks;
-    float timepass = 1f;
-    float addTime = 0.5f;
+    [SerializeField] float TimeClickBlock = 1f;
+   [SerializeField] float TimePushBlocks = 1.5f;
+   [SerializeField] float addTime = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        timepass = Time.time;
+        TimeClickBlock = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Time.time > timepass)
+        if(Time.time > TimeClickBlock)
         {
-            Debug.Log("Worked");
-            timepass = Time.time + addTime;
+          Instantiate(ClickBlock, new Vector3(Random.Range(-8.509f, 9.94f), 5.85f, 0), Quaternion.Euler(0, 0, 0));
+            TimeClickBlock = Time.time + addTime;
+         
+        }
+
+        if(Time.time > TimePushBlocks)
+        {
+
+            Instantiate(PushBlocks, new Vector3(Random.Range(-8.509f, 9.94f), 5.85f, 0), Quaternion.Euler(0, 0, 0));
+            TimePushBlocks = Time.time + addTime;
         }
     }
 }
